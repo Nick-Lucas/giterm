d2ba15ea20f6eee91eb63b55bc72b2023e9918e9
Nick Lucas <me@nicklucas.co.uk>
2019-12-27 15:53:25 +0000

    Prototype graph rewrite, many tests failing

--- app/renderer/lib/gitgraph/src/GitGraphRenderLib/commitsToGraph.js
+++ app/renderer/lib/gitgraph/src/GitGraphRenderLib/commitsToGraph.js
@@ -21,10 +21,11 @@ import _ from 'lodash'
   },
  */
 
-function cloneNodeAsBlank(node) {
+function cloneNodeAsBlank(node, parentSha) {
   return {
     ...node,
     type: 'blank',
+    parents: [parentSha],
   }
 }
 
@@ -32,6 +33,7 @@ function commitToNode(commit, primaryColour, secondaryColour = null) {
   return {
     type: 'node',
     sha: commit.sha,
+    parents: commit.parents,
     deleted: false,
     orphan: commit.parents.length === 0,
     primaryColour,
@@ -48,80 +50,13 @@ export function _link(
   { nodeAtStart = true, nodeAtEnd = true } = {},
 ) {
   return {
-    x1,
+    x1: x1,
     y1,
-    x2,
+    x2: x2,
     y2,
     colour,
     nodeAtStart,
     nodeAtEnd,
-  }
-}
-
-// Keep a track of where children are by their parent shas
-class ChildDirectory {
-  constructor() {
-    // object for parents to look up if any children are searching for them
-    this._lookup = {}
-
-    // object of all children and their parents still being searched for
-    this._tracked = {}
-
-    // Track the number of parents for any commit ever found
-    this._parentCount = {}
-  }
-
-  register = (sha, parentShas, row, column) => {
-    this._tracked[sha] = parentShas
-    this._parentCount[sha] = parentShas.length
-
-    for (let parentIndex = 0; parentIndex < parentShas.length; parentIndex++) {
-      const parentSha = parentShas[parentIndex]
-      if (!this._lookup[parentSha]) {
-        this._lookup[parentSha] = []
-      }
-
-      this._lookup[parentSha].push({ row, column, parentIndex, sha })
-    }
-  }
-
-  lookup = (parentSha) => {
-    return _.sortBy(this._lookup[parentSha] || [], (child) => [
-      child.parentIndex,
-      child.column,
-    ])
-  }
-
-  numberOfParentsFor = (childSha) => {
-    return this._parentCount[childSha] || 0
-  }
-
-  spliceColumn = (column) => {
-    for (const sha in this._lookup) {
-      for (const item of this._lookup[sha]) {
-        if (item.column > column) {
-          item.column -= 1
-        }
-      }
-    }
-  }
-
-  cleanup = (parentSha) => {
-    delete this._lookup[parentSha]
-
-    for (const sha in this._tracked) {
-      const parentIndex = this._tracked[sha].indexOf(parentSha)
-      if (parentIndex >= 0) {
-        this._tracked[sha].splice(parentIndex, 1)
-        if (this._tracked[sha].length === 0) {
-          delete this._tracked[sha]
-        }
-      }
-    }
-  }
-
-  remainingParentsToFind = () => {
-    return _.uniq(_.flatMap(Object.values(this._tracked)))
   }
 }
 
@@ -133,169 +68,106 @@ class ColourTracker {
   next = () => ++this._colourIndex
 }
 
-function rehydrate({ nodes = [], links = [], children = {}, colours = {} }) {
+function rehydrate({ nodes = [], links = [], colours = {} }) {
   return {
     nodes,
     links,
-    children: Object.assign(new ChildDirectory(), children),
     colours: Object.assign(new ColourTracker(), colours),
   }
 }
 
 export function commitsToGraph(commits = [], rehydrationPackage = {}) {
-  const { nodes, links, children, colours } = rehydrate(rehydrationPackage)
+  const { nodes, links, colours } = rehydrate(rehydrationPackage)
 
   function prepareNext() {
     const last = nodes[nodes.length - 1] || []
-    const next = last.map(cloneNodeAsBlank)
-    nodes.push(next)
+    const lastLinks = links[links.length - 1] || []
+    const next = []
+    const rowLinks = []
 
-    // generate initial links
-    const rowNumber = nodes.length - 1
-    const rowLinks = last.map((node, i) =>
-      _link(rowNumber - 1, i, rowNumber, i, node.primaryColour, {
-        nodeAtStart: node.type === 'node',
-        nodeAtEnd: false,
-      }),
-    )
+    const rowIndex = nodes.push(next) - 1
     links.push(rowLinks)
 
-    // Remove deleted nodes which no longer need tracking and update columns
-    for (let i = next.length - 1; i >= 0; i--) {
-      const { deleted, orphan } = next[i]
+    for (const nodeIndexStr in last) {
+      const nodeIndex = parseInt(nodeIndexStr)
+      const node = last[nodeIndex]
+      if (node.deleted || node.orphan) {
+        // TODO: does deleted need any special logic?
 
-      if (deleted || orphan) {
-        next.splice(i, 1)
+        // No longer needs tracking
+        continue
+      }
 
-        rowLinks.splice(i, 1)
-        for (let rl = i; rl < rowLinks.length; rl++) {
-          if (rowLinks[rl].x2 > i) rowLinks[rl].x2 -= 1
+      for (const parentIndexStr in node.parents) {
+        const parentIndex = parseInt(parentIndexStr)
+        const parentSha = node.parents[parentIndex]
+        const columnIndex = next.push(cloneNodeAsBlank(node, parentSha)) - 1
+
+        let colour = node.primaryColour
+        if (node.type === 'node') {
+          if (parentIndex == 0) {
+            colour = node.primaryColour
+          } else {
+            colour = colours.next()
+            node.secondaryColour = colour
+          }
+        } else {
+          colour = lastLinks[nodeIndex].colour
         }
 
-        children.spliceColumn(i)
+        rowLinks.push(
+          _link(rowIndex - 1, nodeIndex, rowIndex, columnIndex, colour, {
+            nodeAtStart: node.type === 'node',
+            nodeAtEnd: false,
+          }),
+        )
       }
     }
 
-    return { last, next, rowNumber, rowLinks }
-  }
-
-  // A merge or branch point might be more than 1 commit away,
-  //  so we have to write links from the merge to the current row
-  function writeLinks(
-    fromY,
-    fromX,
-    toY,
-    toX,
-    colour,
-    { endWithNode = true } = {},
-  ) {
-    const nodeAtEnd = fromY + 1 >= toY
-
-    links[fromY + 1].push(
-      _link(fromY, fromX, fromY + 1, toX, colour, {
-        nodeAtStart: true,
-        nodeAtEnd: nodeAtEnd && endWithNode,
-      }),
-    )
-
-    for (let row = fromY + 1; row < toY; row++) {
-      const nodeAtEnd = row + 1 >= toY
-      links[row + 1].push(
-        _link(row, toX, row + 1, toX, colour, {
-          nodeAtStart: false,
-          nodeAtEnd: nodeAtEnd && endWithNode,
-        }),
-      )
-    }
+    return { last, next, rowIndex, rowLinks }
   }
 
   for (const commit of commits) {
-    const { next, rowNumber, rowLinks } = prepareNext()
+    const { next, rowIndex, rowLinks } = prepareNext()
 
-    function trackNewBranch(childRow = null, childColumn = null) {
+    function trackNewBranch() {
       const colour = colours.next()
-      const column = next.push(commitToNode(commit, colour)) - 1
-      children.register(commit.sha, commit.parents, rowNumber, column)
-
-      if (childRow != null && childColumn != null) {
-        nodes[childRow][childColumn].secondaryColour = colour
-        writeLinks(childRow, childColumn, rowNumber, column, colour)
-      }
+      next.push(commitToNode(commit, colour))
     }
 
     function trackKnownParent(column) {
-      next[column] = commitToNode(commit, next[column].primaryColour)
-      children.register(commit.sha, commit.parents, rowNumber, column)
+      next[column] = commitToNode(commit, rowLinks[column].colour)
+
       rowLinks[column].nodeAtEnd = true
     }
 
-    function trackOtherChildLinks(
-      childSha,
-      childColumn,
-      childRow,
-      destinationColumn,
-      colour,
-    ) {
-      if (childColumn >= next.length) {
-        throw new Error('Invariant: column < next.length evaluated to false')
-      }
-
-      const childHasAnotherParent = children.numberOfParentsFor(childSha) > 1
-      if (childHasAnotherParent) {
-        writeLinks(childRow, childColumn, rowNumber, destinationColumn, colour)
-      } else {
-        next[childColumn].deleted = true
-        rowLinks[childColumn].x2 = destinationColumn
-        rowLinks[childColumn].nodeAtEnd = true
+    const matches = []
+    for (const columnIndexStr in next) {
+      const columnIndex = parseInt(columnIndexStr)
+      const node = next[columnIndex]
+      if (node.parents[0] === commit.sha) {
+        matches.push([columnIndex, node])
       }
     }
 
-    const registeredChildren = children.lookup(commit.sha)
-    if (registeredChildren.length === 0) {
+    if (matches.length === 0) {
       trackNewBranch()
     } else {
-      // Track which column this commit ends up in, as it may be needed later
       let nodeColumn = null
+      for (const matchIndex in matches) {
+        const firstMatch = matchIndex == 0
+        const [columnIndex, node] = matches[matchIndex]
 
-      for (let i = 0; i < registeredChildren.length; i++) {
-        const coords = registeredChildren[i]
-
-        if (i === 0 && coords.parentIndex === 0) {
-          // Found direct parent of existing child
-          nodeColumn = coords.column
-          trackKnownParent(coords.column)
-        } else if (i === 0 && coords.parentIndex !== 0) {
-          // Found merged-in parent
-          nodeColumn = coords.column
-          trackNewBranch(coords.row, coords.column)
+        if (firstMatch) {
+          trackKnownParent(columnIndex)
+          nodeColumn = columnIndex
         } else {
-          // Found child branched off from parent
-          if (nodeColumn == null) {
-            throw "Invariant: nodeColumn was null, ensure it's always populated first"
-          }
-
-          const childNode = nodes[coords.row][coords.column]
-          const colour =
-            coords.parentIndex === 0
-              ? childNode.primaryColour
-              : next[nodeColumn].primaryColour
-          if (childNode.primaryColour !== colour) {
-            childNode.secondaryColour = colour
-          }
-
-          trackOtherChildLinks(
-            coords.sha,
-            coords.column,
-            coords.row,
-            nodeColumn,
-            colour,
-          )
+          rowLinks[columnIndex].nodeAtEnd = true
+          rowLinks[columnIndex].x2 = nodeColumn
+          node.deleted = true
         }
       }
     }
-
-    // Keep the child directory minimal
-    children.cleanup(commit.sha)
   }
 
   // Ensure that even branches we haven't found yet are visible
@@ -332,7 +204,6 @@ export function commitsToGraph(commits = [], rehydrationPackage = {}) {
     rehydrationPackage: {
       nodes: [...nodes],
       links: [...links],
-      children,
       colours,
     },
   }
