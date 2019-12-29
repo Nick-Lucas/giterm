8c3ab274fdbe1bb02b4b3bbbf7664d7af133f974
Nick Lucas <me@nicklucas.co.uk>
2019-12-29 22:31:04 +0000

    Renamed poorly named functions, variables, classes, and add some doc comments to explain process

--- app/renderer/lib/gitgraph/src/GitGraphRenderLib/commitsToGraph.js
+++ app/renderer/lib/gitgraph/src/GitGraphRenderLib/commitsToGraph.js
@@ -29,59 +29,64 @@ export function _link(
   }
 }
 
-class Cursor {
+class BranchTracker {
   constructor() {
-    this.head = []
-    this.workingCopy = []
+    this._commitedLayer = []
+    this._workingLayer = []
   }
 
-  getChild = (column) => {
-    return this.head[column]
-  }
-
-  findChildren = (parentSha) => {
+  /** Find the first child of a given sha, prioritised by its closeness by lineage */
+  findFirstChild = (parentSha) => {
     const children = []
-    for (const column in this.head) {
-      const trackedCommit = this.head[column]
+    for (const column in this._commitedLayer) {
+      const trackedCommit = this._commitedLayer[column]
       if (trackedCommit && trackedCommit.parentSha === parentSha) {
-        children.push([parseInt(column), this.head[column]])
+        children.push([parseInt(column), this._commitedLayer[column]])
       }
     }
 
-    return _.sortBy(children, ([childColumn, child]) => [
-      child.parentIndex,
-      childColumn,
-    ])
+    const firstChild = _.first(
+      _.sortBy(children, ([childColumn, child]) => [
+        child.parentIndex,
+        childColumn,
+      ]),
+    )
+
+    return firstChild || []
   }
 
-  findChildWithParentIndex0 = (parentSha) => {
-    const [firstChild] = this.findChildren(parentSha)
-    if (firstChild && firstChild[1].parentIndex === 0) {
-      return firstChild
+  /** Find the first child of a given sha which considers the parent its first parent */
+  findDirectChild = (parentSha) => {
+    const [firstChildColumn, firstChild] = this.findFirstChild(parentSha)
+    if (firstChild && firstChild.parentIndex === 0) {
+      return [firstChildColumn, firstChild]
     }
     return []
   }
 
+  /** Mark all children as found for a given parent */
   markParentFound = (parentSha) => {
-    for (const child of this.workingCopy) {
+    for (const child of this._workingLayer) {
       if (child && child.parentSha === parentSha) {
         child.parentFound = true
       }
     }
   }
 
+  /** Unassign all children which have been found from the tracker */
   immediatelyUnassignFoundColumns = () => {
-    for (const index in this.workingCopy) {
-      if (!this.workingCopy[index]) {
+    for (const index in this._workingLayer) {
+      if (!this._workingLayer[index]) {
         continue
       }
-      const { parentFound, orphan } = this.workingCopy[index]
+      const { parentFound, orphan } = this._workingLayer[index]
       if (parentFound || orphan) {
-        this.workingCopy[index] = undefined
+        this._workingLayer[index] = undefined
       }
     }
   }
 
+  /** Update a child with its direct parent and look for its grand-parent instead */
   updateColumn = (
     column,
     parentSha,
@@ -89,7 +94,7 @@ class Cursor {
     colour,
     { symlink = null } = {},
   ) => {
-    this.workingCopy[column] = {
+    this._workingLayer[column] = {
       isNode: true,
       sha: commit.sha,
       parentSha,
@@ -103,10 +108,11 @@ class Cursor {
     return column
   }
 
+  /** Assign a column to a newly found child and look for its parent */
   assignColumn = (parentSha, commit, colour, { symlink = null } = {}) => {
     let column = 0
-    for (; column <= this.workingCopy.length; column++) {
-      if (this.workingCopy[column] === undefined) {
+    for (; column <= this._workingLayer.length; column++) {
+      if (this._workingLayer[column] === undefined) {
         break
       }
     }
@@ -116,6 +122,7 @@ class Cursor {
     })
   }
 
+  /** Move the tracker forward ready for new commit and generate links for all tracked children */
   next = () => {
     const links = []
 
@@ -123,8 +130,8 @@ class Cursor {
     this.immediatelyUnassignFoundColumns()
 
     // Commit to head
-    this.head = this.workingCopy
-    this.workingCopy = this.workingCopy.map(
+    this._commitedLayer = this._workingLayer
+    this._workingLayer = this._workingLayer.map(
       (columnObj) =>
         columnObj && {
           ...columnObj,
@@ -134,9 +141,9 @@ class Cursor {
     )
 
     // Generate links
-    for (const columnStr in this.head) {
+    for (const columnStr in this._commitedLayer) {
       const column = parseInt(columnStr)
-      const columnObj = this.head[column]
+      const columnObj = this._commitedLayer[column]
       if (!columnObj) {
         continue
       }
@@ -171,10 +178,10 @@ class ColourTracker {
 }
 
 class GraphState {
-  constructor(nodes, links, cursor) {
+  constructor(nodes, links, branchTracker) {
     this.nodes = nodes
     this.links = links
-    this.cursor = cursor
+    this.branchTracker = branchTracker
 
     this._bufferedLinks = []
   }
@@ -183,11 +190,13 @@ class GraphState {
     return this.nodes.push(node)
   }
 
+  /** Buffers a link which has been discovered for the next layer. Will be appended in the next layer */
   addLinkForNextRow(link) {
     this._bufferedLinks.push(link)
   }
 
-  setLinkDestinations(destinationNode) {
+  /** Once a parent is found, generated links from the current layer need updating to point to it */
+  setChildLinkDestinations(destinationNode) {
     if (_.last(this.nodes) !== destinationNode) {
       throw new Error(
         'Coding Error: destination node is not the latest node in the graph',
@@ -202,10 +211,11 @@ class GraphState {
     }
   }
 
-  prepareNext = () => {
+  /** Prepare the graph for a new commit to be worked on */
+  prepareNextRow = () => {
     // Move cursor foward to retrieve auto-generated links
-    const autoLinks = this.cursor.next(this.nodes.length)
-    const rowLinks = [...autoLinks, ...this._bufferedLinks]
+    const autoLinks = this.branchTracker.next(this.nodes.length)
+    const rowLinks = [...this._bufferedLinks, ...autoLinks]
 
     // Update state for next cycle
     this._bufferedLinks = []
@@ -221,47 +231,47 @@ class GraphState {
 function rehydrate({
   nodes: nodesData = [],
   links: linksData = [],
-  cursor: cursorData = {},
+  branchTracker: branchTrackerData = {},
   colours: coloursData = {},
 }) {
-  const cursor = Object.assign(new Cursor(), cursorData)
+  const branchTracker = Object.assign(new BranchTracker(), branchTrackerData)
   const colours = Object.assign(new ColourTracker(), coloursData)
 
   return {
-    graph: new GraphState(nodesData, linksData, cursor),
-    cursor,
+    graph: new GraphState(nodesData, linksData, branchTracker),
+    branchTracker,
     colours,
   }
 }
 
 export function commitsToGraph(commits = [], rehydrationPackage = {}) {
-  const { graph, cursor, colours } = rehydrate(rehydrationPackage)
+  const { graph, branchTracker, colours } = rehydrate(rehydrationPackage)
 
   for (const commit of commits) {
-    graph.prepareNext()
+    graph.prepareNextRow()
 
     function trackOtherParents(node) {
-      cursor.immediatelyUnassignFoundColumns()
+      branchTracker.immediatelyUnassignFoundColumns()
 
-      for (const parentSha of commit.parents.slice(1)) {
-        let colour = null
+      const otherParentShas = commit.parents.slice(1)
+      for (const parentSha of otherParentShas) {
+        const [directChildColumn, directChild] = branchTracker.findDirectChild(
+          parentSha,
+        )
 
-        const [
-          firstChildOfParentColumn,
-          firstChildOfParent,
-        ] = cursor.findChildWithParentIndex0(parentSha)
-
-        if (firstChildOfParent && firstChildOfParent.parentIndex === 0) {
+        if (directChild && directChild.parentIndex === 0) {
           // If this is a merge in from another branch with a colour already chosen
-          colour = firstChildOfParent.colour
+          const colour = directChild.colour
+
+          node.secondaryColour = colour
 
           graph.addLinkForNextRow(
             _link(
               node.column,
-              firstChildOfParentColumn,
+              directChildColumn,
               colour,
               node.sha,
-              firstChildOfParent.parentSha,
+              directChild.parentSha,
               {
                 nodeAtStart: true,
                 nodeAtEnd: false,
@@ -270,70 +280,61 @@ export function commitsToGraph(commits = [], rehydrationPackage = {}) {
           )
         } else {
           // If this is a merge in from a new branch not yet discovered
-          colour = colours.next()
+          const colour = colours.next()
+
+          node.secondaryColour = colour
 
           // Assign column to parent, with a symlink so
           //  auto-generated links point back to the current node
-          cursor.assignColumn(parentSha, commit, colour, {
+          branchTracker.assignColumn(parentSha, commit, colour, {
             symlink: node.column,
           })
         }
-
-        node.secondaryColour = colour
       }
     }
 
     function trackNewBranch() {
-      let node = null
       const colour = colours.next()
       const parentSha = commit.parents[0]
+      const column = branchTracker.assignColumn(parentSha, commit, colour)
 
-      const column = cursor.assignColumn(parentSha, commit, colour)
-
-      node = commitToNode(commit, column, colour)
+      const node = commitToNode(commit, column, colour)
       graph.addNode(node)
-
-      if (commit.parents.length > 1) {
-        trackOtherParents(node)
-      }
 
       return node
     }
 
-    const children = cursor.findChildren(commit.sha)
-    if (children.length === 0) {
-      trackNewBranch()
+    function updateTrackedBranch(childColumn, child) {
+      const colour = child.colour
+
+      const node = commitToNode(commit, childColumn, colour)
+      graph.addNode(node)
+
+      branchTracker.updateColumn(
+        node.column,
+        commit.parents[0],
+        commit,
+        node.primaryColour,
+      )
+
+      return node
+    }
+
+    const [childColumn, child] = branchTracker.findFirstChild(commit.sha)
+    if (child == null) {
+      const node = trackNewBranch()
+      if (commit.parents.length > 1) {
+        trackOtherParents(node)
+      }
     } else {
-      // Track known parent which may belong to 1 or more children
-      let node = null
-      for (const [childColumn, child] of children) {
-        cursor.markParentFound(commit.sha)
+      branchTracker.markParentFound(commit.sha)
 
-        if (node === null) {
-          const colour = child.colour
-
-          node = commitToNode(commit, childColumn, colour)
-          graph.addNode(node)
-
-          cursor.updateColumn(
-            node.column,
-            commit.parents[0],
-            commit,
-            node.primaryColour,
-          )
-
-          if (commit.parents.length > 1) {
-            trackOtherParents(node)
-          }
-        }
+      const node = updateTrackedBranch(childColumn, child)
+      if (commit.parents.length > 1) {
+        trackOtherParents(node)
       }
 
-      if (!node) {
-        // TODO: Need to handle new branches appearing
-        throw 'Coding error: NODE SHOULD BE SET'
-      }
-
-      graph.setLinkDestinations(node)
+      graph.setChildLinkDestinations(node)
     }
   }
 
@@ -341,13 +342,12 @@ export function commitsToGraph(commits = [], rehydrationPackage = {}) {
 
   return {
     nodes,
-    // TODO: reverse this at a better time, or insert links in the best order for presentation to begin with
-    links: links.map((rowLinks) => _.reverse(rowLinks)),
+    links,
     commits,
     rehydrationPackage: {
       nodes: [...nodes],
       links: [...links],
-      cursor,
+      branchTracker,
       colours,
     },
   }
