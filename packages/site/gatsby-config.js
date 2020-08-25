f27c943e26bd21bcf0c4107e66cc0d416b441046
Nick Lucas <me@nicklucas.co.uk>
2020-08-25 21:50:04 +0100

    Ensure build properly prefixes paths for github pages

--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: process.env.PATH_PREFIX,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
