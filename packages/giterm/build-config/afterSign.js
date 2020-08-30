e1c4d09a7a03cbf49ed1b37b7f16864ad7ab5da6
Nick Lucas <me@nicklucas.co.uk>
2020-08-30 12:42:39 +0100

    Move notarization to only require within darwin build

--- packages/giterm/build-config/afterSign.js
+++ packages/giterm/build-config/afterSign.js
@@ -1,10 +1,8 @@
-// require('dotenv').config();
-const { notarize } = require('electron-notarize')
-
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context
 
   if (electronPlatformName === 'darwin') {
+    const { notarize } = require('electron-notarize')
     const appName = context.packager.appInfo.productFilename
 
     return await notarize({
