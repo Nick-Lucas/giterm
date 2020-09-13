ce311d7efbf00c6f501e525833510e01731a3935
Nick Lucas <me@nicklucas.co.uk>
2020-09-13 17:14:31 +0100

    Open Giterm on active screen

--- packages/giterm/app/main/index.js
+++ packages/giterm/app/main/index.js
@@ -1,5 +1,5 @@
 import path from 'path'
-import { app, BrowserWindow, Menu } from 'electron'
+import { app, BrowserWindow, Menu, screen } from 'electron'
 import logger from 'electron-log'
 import { autoUpdater } from 'electron-updater'
 import getMenu from './menu'
@@ -66,6 +66,13 @@ app.on('ready', async () => {
       preload: path.join(__dirname, '../sentry.js'),
     },
   })
+
+  // Show on currently active screen
+  const currentScreen = screen.getDisplayNearestPoint(
+    screen.getCursorScreenPoint(),
+  )
+  mainWindow.setBounds(currentScreen.workArea)
+
   mainWindow.maximize()
 
   mainWindow.loadFile(
