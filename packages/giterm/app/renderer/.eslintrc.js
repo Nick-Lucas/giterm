0e5feec7d1d943a34a2f2ee68acff66bce147a33
Nick Lucas <me@nicklucas.co.uk>
2021-10-29 20:26:42 +0100

    Ban child_process in the renderer

--- packages/giterm/app/renderer/.eslintrc.js
+++ packages/giterm/app/renderer/.eslintrc.js
@@ -2,6 +2,25 @@ module.exports = {
   env: {
     browser: true,
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      rules: {
+        'no-restricted-imports': [
+          'error',
+          {
+            paths: [
+              {
+                name: 'child_process',
+                message:
+                  'No child_process in renderer due to performance problems. Use or expand the IPC worker found in /app/main',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
   settings: {
     'import/resolver': {
       alias: {
