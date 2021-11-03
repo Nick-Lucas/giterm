7674f05c87e2977527b6399c138f9ec9b6d539a0
Nick Lucas <me@nicklucas.co.uk>
2021-11-03 21:47:13 +0000

    Exclude giterm from unit testing in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
-const packageJson = require('./package.json')
+const fs = require('fs')
+const path = require('path')
+
+const excludeProjects = ['giterm-e2e']
+
+const packagesDir = path.join(__dirname, 'packages')
+const projectNames = fs
+  .readdirSync(packagesDir, {
+    withFileTypes: true,
+  })
+  .filter((file) => file.isDirectory())
+  .map((dir) => dir.name)
+  .filter((dirName) => !excludeProjects.includes(dirName))
+  .map((dirName) => path.join('packages', dirName))
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  projects: packageJson.workspaces.packages,
+  projects: projectNames,
 }
